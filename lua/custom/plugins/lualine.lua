5c2e0f4bdcb6f7fa30fc229463355e01e78de960
myroslavsuprun <myroslavsuprun@gmail.com>
2026-03-28 16:12:52 +0200

    built-in plugin manager

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,41 +1,37 @@
-return {
-  'nvim-lualine/lualine.nvim',
-  event = 'VimEnter',
-  opts = {
-    options = {
-      icons_enabled = true,
-      theme = 'auto',
-      component_separators = '|',
-      section_separators = { left = '', right = '' },
-    },
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = '|',
+    section_separators = { left = '', right = '' },
+  },
 
-    sections = {
-      lualine_a = {
-        {
-          'mode',
-          fmt = function(str)
-            return str:sub(1, 1)
-          end,
-          separator = { left = '' },
-          right_padding = 2,
-        },
+  sections = {
+    lualine_a = {
+      {
+        'mode',
+        fmt = function(str)
+          return str:sub(1, 1)
+        end,
+        separator = { left = '' },
+        right_padding = 2,
       },
-      lualine_b = {
-        'branch',
-        'diff',
-        'diagnostics',
+    },
+    lualine_b = {
+      'branch',
+      'diff',
+      'diagnostics',
+    },
+    lualine_c = {
+      {
+        'filename',
+        path = 1,
       },
-      lualine_c = {
-        {
-          'filename',
-          path = 1,
-        },
-      },
-      lualine_x = {},
-      lualine_y = { 'filetype', 'progress' },
-      lualine_z = {
-        { 'location', separator = { right = '' }, left_padding = 2 },
-      },
+    },
+    lualine_x = {},
+    lualine_y = { 'filetype', 'progress' },
+    lualine_z = {
+      { 'location', separator = { right = '' }, left_padding = 2 },
     },
   },
 }
